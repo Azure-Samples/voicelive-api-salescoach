b39ce9f3880f5f69251e64ac8d402cdb4afdf3c3
Damiano Curia <damiano.curia@gmail.com>
2025-09-02 13:16:50 +0000

    Fixed devcontainer virtual environment

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,7 +3,7 @@
 
 cd backend
 python3 -m venv .venv
-source .venv/bin/activate
+. .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements-test.txt
 pip install -r requirements.txt
